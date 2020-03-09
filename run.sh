10318992b48d7e27c3b4408dcaf842faf60e3e3b
martinhetschel <martin.hetschel@mach1kart.de>
2020-03-10 00:17:13 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 node /theia/src-gen/backend/main.js /data --hostname=0.0.0.0 --port=4444 &
-node /usr/src/node-red/node_modules/node-red/red.js --userDir /data &
 node startMqttTimer &
 node startWebServer &
+node /usr/src/node-red/node_modules/node-red/red.js --userDir /data
