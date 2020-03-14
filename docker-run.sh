624fe0b39825bcc6e844bb45224feb29d3aae1f7
martinhetschel <martin.hetschel@mach1kart.de>
2020-03-14 16:11:40 +0100

    Delete docker-run.sh

--- docker-run.sh
+++
@@ -1,7 +0,0 @@
-cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-echo "Europe/Berlin" >  /etc/timezone
-
-node /theia/src-gen/backend/main.js /home/node --hostname=0.0.0.0 --port=4444 &
-node startMqttTimer &
-node startWebServer &
-node /usr/src/node-red/node_modules/node-red/red.js --userDir /data
