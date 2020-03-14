02ff06fdbf1ce25bac397124223f67889c2acc3b
mh <mh@qxf.de>
2020-03-14 20:26:52 +0000

    update

--- getAM2H-ServerV2.sh
+++ getAM2H-ServerV2.sh
@@ -5,7 +5,7 @@
 mkdir AM2H-ServerV2
 cd AM2H-ServerV2
 git clone https://github.com/AM2H-Development/AM2H-ServerV2.git base
-git clone https://github.com/AM2H-Development/AM2H-ServerV2-userdata.git user
+# git clone https://github.com/AM2H-Development/AM2H-ServerV2-userdata.git user
 cd base
 git pull
 npm install
