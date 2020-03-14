8c17172e1eb1d5ae70e9ff1c93a080a256bf5c85
martinhetschel <martin.hetschel@mach1kart.de>
2020-03-14 21:37:55 +0100

    Update getAM2H-ServerV2.sh

--- install/getAM2H-ServerV2.sh
+++ install/getAM2H-ServerV2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Install by typing:
-# wget -q -O - https://raw.githubusercontent.com/AM2H-Development/AM2H-ServerV2/2.1.0/getAM2H-ServerV2.sh | sh
+# wget -q -O - https://raw.githubusercontent.com/AM2H-Development/AM2H-ServerV2/2.1.0/install/getAM2H-ServerV2.sh | bash -s https://github.com/AM2H-Development/AM2H-ServerV2-userdata.git
 
 mkdir AM2H-ServerV2
 cd AM2H-ServerV2
 git clone https://github.com/AM2H-Development/AM2H-ServerV2.git base
-# git clone https://github.com/AM2H-Development/AM2H-ServerV2-userdata.git user
+git clone $1 user
 cd base
 git pull
 npm install
